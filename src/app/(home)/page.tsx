6c37210588e5c12aa72bc0bca0dcf71594587c64
Pustam Rai <github.pustam@gmail.com>
2025-05-21 21:20:57 +0545

    add AddBook component and integrate it into the Home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,11 @@
 import { Suspense } from "react";
 import BookList from "./components/BookList";
 import LoadingSkeleton from "@/components/LoadingSkeleton";
+import AddBook from "./components/AddBook";
 
 export default function Home() {
   return (
-    <div className="mt-12">
+    <div className="mt-12 min-h-screen relative">
       <h2 className="text-center text-orange-500 font-bold text-2xl font-mono">
         Welcome to eBook store
       </h2>
@@ -12,6 +13,10 @@ export default function Home() {
       <Suspense fallback={<LoadingSkeleton count={3} />}>
         <BookList />
       </Suspense>
+
+      <div className="fixed z-10 bottom-6 left-[840px] right-0">
+        <AddBook />
+      </div>
     </div>
   );
 }
