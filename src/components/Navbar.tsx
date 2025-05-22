5f6c8979c0eb986e0d071d89a1f34b8b1372c322
Pustam Rai <github.pustam@gmail.com>
2025-05-22 08:54:32 +0545

    add signup page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,9 +16,12 @@ function Navbar() {
           <button className="h-10 rounded-md border border-orange-500 px-4 py-2 text-sm font-medium text-orange-900 transition-all hover:border-orange-500 hover:bg-orange-500 active:border-black active:bg-blue-500 cursor-pointer">
             Sign in
           </button>
-          <button className="h-10 rounded-md bg-primary-500 px-4 py-2 text-sm font-medium text-gray-800 transition-all bg-orange-300 hover:bg-orange-500 active:bg-blue-500 cursor-pointer">
-            Sign up
-          </button>
+
+          <Link href={"/signup"}>
+            <button className="h-10 rounded-md bg-primary-500 px-4 py-2 text-sm font-medium text-gray-800 transition-all bg-orange-300 hover:bg-orange-500 active:bg-blue-500 cursor-pointer">
+              Sign up
+            </button>
+          </Link>
         </div>
       </div>
     </nav>
