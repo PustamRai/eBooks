1a3506b0ee51ddf0dd0e6e052eeaf57415fe16fd
Pustam Rai <github.pustam@gmail.com>
2025-05-22 21:50:52 +0545

    update cookies structure

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -45,14 +45,12 @@ export async function POST(request: NextRequest) {
       { expiresIn: "1d" }
     );
 
-    const options = {
+    (await cookies()).set("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 24 * 60 * 60, // 1 day
-    };
-
-    cookies().set("token", token, options);
+    });
 
     const loggedInUser = await User.findById(user._id).select("-password");
 
