3ae6b323930ee4badf3f605b1912b635909c0723
Pustam Rai <github.pustam@gmail.com>
2025-05-22 23:05:05 +0545

    add LogoutButton component and integrate it into Navbar for user logout functionality

---
+++ src/components/LogoutButton.tsx
@@ -0,0 +1,35 @@
+"use client";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import React from "react";
+import toast from "react-hot-toast";
+
+function Logout() {
+  const router = useRouter();
+
+  const logout = async () => {
+    try {
+      await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/users/logout`);
+
+      toast.success("Logout successful");
+      router.refresh();
+      router.push("/");
+    } catch (error) {
+      console.log("Error in logout: ", error);
+      toast.error(error.response?.data?.message || "Logout failed");
+    }
+  };
+  return (
+    <div>
+      <button
+        type="submit"
+        className="h-10 rounded-md border border-red-500 px-4 py-2 text-sm font-medium text-red-700 hover:bg-red-500 hover:text-white transition"
+        onClick={logout}
+      >
+        Logout
+      </button>
+    </div>
+  );
+}
+
+export default Logout;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,14 @@
+import { cookies } from "next/headers";
 import Link from "next/link";
 import React from "react";
+import Logout from "./LogoutButton";
 
 function Navbar() {
+  const cookieStore = cookies();
+  const token = cookieStore.get("token")?.value;
+
+  const isLoggedIn = !!token; // set isloggedIn true, if token exists, otherwise false
+
   return (
     <nav className="border-b border-gray-400">
       <div className="max-w-7xl mx-auto flex items-center justify-between py-4">
@@ -12,18 +19,25 @@ function Navbar() {
             </div>
           </Link>
         </div>
-        <div className="flex items-center gap-4">
-          <Link href={"/login"}>
-            <button className="h-10 rounded-md border border-orange-500 px-4 py-2 text-sm font-medium text-orange-900 transition-all hover:border-orange-500 hover:bg-orange-500 active:border-black active:bg-blue-500 cursor-pointer">
-              Sign in
-            </button>
-          </Link>
 
-          <Link href={"/signup"}>
-            <button className="h-10 rounded-md bg-primary-500 px-4 py-2 text-sm font-medium text-gray-800 transition-all bg-orange-300 hover:bg-orange-500 active:bg-blue-500 cursor-pointer">
-              Sign up
-            </button>
-          </Link>
+        <div className="flex items-center gap-4">
+          {isLoggedIn ? (
+            <Logout />
+          ) : (
+            <div>
+              <Link href={"/login"}>
+                <button className="h-10 rounded-md border border-orange-500 px-4 py-2 text-sm font-medium text-orange-900 transition-all hover:border-orange-500 hover:bg-orange-500 active:border-black active:bg-blue-500 cursor-pointer">
+                  Sign in
+                </button>
+              </Link>
+
+              <Link href={"/signup"}>
+                <button className="h-10 rounded-md bg-primary-500 px-4 py-2 text-sm font-medium text-gray-800 transition-all bg-orange-300 hover:bg-orange-500 active:bg-blue-500 cursor-pointer">
+                  Sign up
+                </button>
+              </Link>
+            </div>
+          )}
         </div>
       </div>
     </nav>
