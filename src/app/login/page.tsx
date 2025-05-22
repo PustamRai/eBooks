978e464f1ad4a8d1f2db4240a803a893e8ea15a6
Pustam Rai <github.pustam@gmail.com>
2025-05-22 08:59:43 +0545

    add login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,69 @@
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+
+function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen  p-4 mx-auto">
+      <div className="max-w-md w-full px-6 py-8 rounded-lg ">
+        <h2 className="text-2xl font-normal text-center text-gray-800 mb-6 font-serif">
+          Login<span className="mx-2">—</span>
+        </h2>
+
+        <form onSubmit={handleSubmit}>
+          <div className="space-y-4">
+            <div>
+              <input
+                type="email"
+                placeholder="Email"
+                className="w-full px-3 py-2 border border-gray-500 rounded-xs focus:outline-none focus:ring-1 focus:ring-black"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+
+            <div>
+              <input
+                type="password"
+                placeholder="Password"
+                className="w-full px-3 py-2 border border-gray-500 rounded-xs focus:outline-none focus:ring-1 focus:ring-black"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+          </div>
+
+          <div className="flex justify-between items-center text-sm mt-2 mb-6">
+            <div className="text-gray-600 hover:underline">
+              Forgot your password?
+            </div>
+
+            <Link href={"/signup"}>
+              <div className="text-gray-600 hover:underline flex items-center">
+                Create account
+              </div>
+            </Link>
+          </div>
+
+          <button
+            type="submit"
+            className="w-full md:w-auto md:px-12 py-2 bg-black text-white text-sm font-medium rounded-xs hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-500 mx-auto block cursor-pointer"
+          >
+            Sign in
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,9 +13,11 @@ function Navbar() {
           </Link>
         </div>
         <div className="flex items-center gap-4">
-          <button className="h-10 rounded-md border border-orange-500 px-4 py-2 text-sm font-medium text-orange-900 transition-all hover:border-orange-500 hover:bg-orange-500 active:border-black active:bg-blue-500 cursor-pointer">
-            Sign in
-          </button>
+          <Link href={"/login"}>
+            <button className="h-10 rounded-md border border-orange-500 px-4 py-2 text-sm font-medium text-orange-900 transition-all hover:border-orange-500 hover:bg-orange-500 active:border-black active:bg-blue-500 cursor-pointer">
+              Sign in
+            </button>
+          </Link>
 
           <Link href={"/signup"}>
             <button className="h-10 rounded-md bg-primary-500 px-4 py-2 text-sm font-medium text-gray-800 transition-all bg-orange-300 hover:bg-orange-500 active:bg-blue-500 cursor-pointer">
