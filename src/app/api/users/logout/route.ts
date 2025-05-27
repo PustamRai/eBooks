e4f7f26cdcd2c8c2140aa2635e19df939764f84b
Pustam Rai <github.pustam@gmail.com>
2025-05-27 09:16:56 +0545

    refactor logout function to use request object and improve cookie handling

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -1,29 +1,33 @@
-import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST() {
+export async function POST(request: NextRequest) {
   try {
-    (await cookies()).set("token", "", {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-      maxAge: 0, // Immediately expire
-      path: "/",
-    });
+    const token = request.cookies.get("token")?.value;
 
-    return NextResponse.json(
+    const response = NextResponse.json(
       {
         success: true,
         message: "Logged out successfully",
+        data: token,
       },
       { status: 200 }
     );
+
+    // clear the cookie
+    response.cookies.set("token", "", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      expires: new Date(0),
+    });
+
+    return response;
   } catch (error) {
     console.log("Error in logout: ", error);
     return NextResponse.json(
       {
         success: false,
-        message: "Failed logout",
+        message: "Logout Failed",
         error: (error as Error).message,
       },
       { status: 500 }
